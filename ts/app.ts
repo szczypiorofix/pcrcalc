442910fc4065c0adac2516770ba994d6a941c921
szczypiorofix <szczypiorofix@o2.pl>
2018-05-22 15:16:08 +0200

    Dodawanie do localStorage i wyświetlanie historii.

--- ts/app.ts
+++ ts/app.ts
@@ -4,22 +4,20 @@
 //     });
 // }
 
-document.getElementById("saveoffline").addEventListener('click', function(event) {
-    // event.preventDefault();
-    // caches.open('PCR-calc.v-1.00.000').then(function(cache) {
-    //     fetch('./help.json').then(function(response) {
-    //         // /get-article-urls returns a JSON-encoded array of
-    //         // resource URLs that a given article depends on
-    //         console.log('OK.');
-    //         //console.log(response);
-    //         return response.json();
-    //     }).then(function(urls) {
-    //         console.log('Zapisywanie na potem...');
-    //         console.log(urls); // Zwraca treści plików wpisane w 'help.json'
-    //         cache.addAll(urls['cache']);
-    //     });
-    // });
-
-    
-});
+// document.getElementById("saveoffline").addEventListener('click', function(event) {
+//     event.preventDefault();
+//     caches.open('PCR-calc.v-1.00.000').then(function(cache) {
+//         fetch('./help.json').then(function(response) {
+//             // /get-article-urls returns a JSON-encoded array of
+//             // resource URLs that a given article depends on
+//             console.log('OK.');
+//             //console.log(response);
+//             return response.json();
+//         }).then(function(urls) {
+//             console.log('Zapisywanie na potem...');
+//             console.log(urls); // Zwraca treści plików wpisane w 'help.json'
+//             cache.addAll(urls['cache']);
+//         });
+//     });
+// });
 
--- ts/script.ts
+++ ts/script.ts
@@ -124,12 +124,7 @@ class PCRCalc {
 
         
         if (typeof(Storage) !== "undefined") {
-            // Code for localStorage/sessionStorage.
-            
-            this.dataForStorage['fields'] = {
-                // TODO Dodawanie na bierząco ostatnich wartości pól - jako lastResults w localStorage
-                // TODO Dodawanie po kliknięciu w + do historii wyników (wartości pól, data oraz nazwa)
-
+            this.dataForStorage = {
                 'inputMaxVolume': this.inputFields['inputMaxVolume'].value,
                 'inputH2O': this.inputFields['inputH2O'].value,
                 'inputBuffer': this.inputFields['inputBuffer'].value,
@@ -141,6 +136,7 @@ class PCRCalc {
                 'inputDNTPs': this.inputFields['inputDNTPs'].value,
                 'inputDNA': this.inputFields['inputDNA'].value,
                 'outputMaxVolume': this.inputFields['outputMaxVolume'].value,
+                'numberOfProbes': this.inputFields['numberOfProbes'].value,
                 'inputDifferenceValue': this.resultFields['inputDifferenceValue'].innerHTML,
                 'outputH2O': this.resultFields['outputH2O'].innerHTML,
                 'outputH2OMax': this.resultFields['outputH2OMax'].innerHTML,
@@ -155,22 +151,163 @@ class PCRCalc {
                 'outputPrimer2': this.resultFields['outputPrimer2'].innerHTML,
                 'outputPrimer2Max': this.resultFields['outputPrimer2Max'].innerHTML,
                 'outputDNTPs': this.resultFields['outputDNTPs'].innerHTML,
+                'outputDNTPsMax': this.resultFields['outputDNTPsMax'].innerHTML,
                 'outputPolymerase': this.resultFields['outputPolymerase'].innerHTML,
                 'outputPolymeraseMax': this.resultFields['outputPolymeraseMax'].innerHTML,
-                'outputDNA': this.resultFields['outputDNA'].innerHTML
-                
+                'outputDNA': this.resultFields['outputDNA'].innerHTML,
+                'updateDate': new Date().toLocaleString()
             };
             localStorage.setItem("pcrCalcLatest", JSON.stringify(this.dataForStorage));
         } else {
-            // Sorry! No Web Storage support..
+            console.error("Ta przeglądarka nie obsługuje localStorage!");
         }
+    }
 
+    private options(data:Array<string>) {
+        let self = this;
+        let mainContainer = document.createElement('div');
+        let mainStyle = document.createElement('style');
+        let buttonClose = document.createElement("button");
+        buttonClose.appendChild(document.createTextNode("X"));
+        buttonClose.className = "close";
+        buttonClose.addEventListener('click', function(event) {
+            mainContainer.remove();
+        });
+        mainContainer.appendChild(buttonClose);
 
-        
+        mainContainer.className = 'historyMainDiv';
+        mainStyle.innerHTML = 
+        '.historyMainDiv { position: fixed; display: block; top: 5%; left: 5%; width: 90%; height: 90%; background-color: rgba(50, 50, 50, 0.9);} ' +
+        ' .historyMainDiv button {display: block; margin: 10px 5px; padding: 10px 25px; background-color: #FF4081; color: #FFFFFF; border: none; border-radius: 3px;} ' +
+        ' .historyMainDiv button:hover {cursor: pointer;}' +
+        ' .historyMainDiv button.close { position: absolute; top: 1px; right: 1px; background-color: #555555; color: #eeeeee; padding: 5px 10px;}' +
+        ' .historyMainDiv button.close:hover {background-color: #454545; cursor: pointer;}';
+        for (let i = 0; i < data.length; i++) {
+            //console.log(data[i]['name']+" "+data[i]['updateDate']);
+            let b = document.createElement("button");
+            let bText = document.createTextNode(data[i]['name']+" "+data[i]['updateDate']);
+            b.appendChild(bText);
+            b.id = 'load'+i;
+            let deleteB = document.createElement('span');
+            deleteB.appendChild(document.createTextNode("x"));
+
+            b.addEventListener('click', function(event) {
+                
+                let e = <HTMLElement>event.currentTarget;
+                let elId = e.id.substr(4); // removing 'load' from id
+                let el = parseInt(elId);
+                //console.log(el);
+
+                self.inputFields['inputMaxVolume'].value = data[el]['inputMaxVolume'];
+                self.inputFields['inputH2O'].value = data[el]['inputH2O'];
+                self.inputFields['inputBuffer'].value = data[el]['inputBuffer'];
+                self.inputFields['inputEnhancer'].value = data[el]['inputEnhancer'];
+                self.inputFields['inputMgCl2'].value = data[el]['inputMgCl2'];
+                self.inputFields['inputPrimer1'].value = data[el]['inputPrimer1'];
+                self.inputFields['inputPrimer2'].value = data[el]['inputPrimer2'];
+                self.inputFields['inputPolymerase'].value = data[el]['inputPolymerase'];
+                self.inputFields['inputDNTPs'].value = data[el]['inputDNTPs'];
+                self.inputFields['inputDNA'].value = data[el]['inputDNA'];
+                self.inputFields['outputMaxVolume'].value = data[el]['outputMaxVolume'];
+                self.inputFields['numberOfProbes'].value = data[el]['numberOfProbes'];
+                self.resultFields['inputDifferenceValue'].innerHTML = data[el]['inputDifferenceValue'];
+                self.resultFields['outputH2O'].innerHTML = data[el]['outputH2O'];
+                self.resultFields['outputH2OMax'].innerHTML = data[el]['outputH2OMax'];
+                self.resultFields['outputBuffer'].innerHTML = data[el]['outputBuffer'];
+                self.resultFields['outputBufferMax'].innerHTML = data[el]['outputBufferMax'];
+                self.resultFields['outputEnhancer'].innerHTML = data[el]['outputEnhancer'];
+                self.resultFields['outputEnhancerMax'].innerHTML = data[el]['outputEnhancerMax'];
+                self.resultFields['outputMgCl2'].innerHTML = data[el]['outputMgCl2'];
+                self.resultFields['outputMgCl2Max'].innerHTML = data[el]['outputMgCl2Max'];
+                self.resultFields['outputPrimer1'].innerHTML = data[el]['outputPrimer1'];
+                self.resultFields['outputPrimer1Max'].innerHTML = data[el]['outputPrimer1Max'];
+                self.resultFields['outputPrimer2'].innerHTML = data[el]['outputPrimer2'];
+                self.resultFields['outputPrimer2Max'].innerHTML = data[el]['outputPrimer2Max'];
+                self.resultFields['outputDNTPs'].innerHTML = data[el]['outputDNTPs'];
+                self.resultFields['outputDNTPsMax'].innerHTML = data[el]['outputDNTPsMax'];
+                self.resultFields['outputPolymerase'].innerHTML = data[el]['outputPolymerase'];
+                self.resultFields['outputPolymeraseMax'].innerHTML = data[el]['outputPolymeraseMax'];
+                self.resultFields['outputDNA'].innerHTML = data[el]['outputDNA'];
+
+                mainContainer.remove();
+            });
+
+            mainContainer.appendChild(b);
+            mainContainer.appendChild(deleteB);
+        }
+        mainContainer.appendChild(mainStyle);
+        document.getElementsByTagName('body')[0].appendChild(mainContainer);
+    }
+
+    private addListener() {
+        let self = this;
+        document.getElementById('loadoffline').addEventListener('click', function(event) {
+            if (typeof(Storage) !== "undefined") {
+                let dataFromStorage = JSON.parse(localStorage.getItem('pcrCalcHistory'));
+                if (dataFromStorage === null) {
+                    localStorage.setItem('pcrCalcHistory', '[]');
+                    dataFromStorage = [];
+                }
+                //console.log(dataFromStorage);
+                self.options(dataFromStorage);
+            } else {
+                console.error("Ta przeglądarka nie obsługuje localStorage!");
+            }    
+        });
+        document.getElementById("saveoffline").addEventListener('click', function(event) {
+            if (typeof(Storage) !== "undefined") {
+                let dataName = prompt("Podaj nazwę zestawu: ");
+                self.dataForStorage['fields'] = {
+                    'name': dataName,
+                    'updateDate': new Date().toLocaleString(),
+                    'inputMaxVolume': self.inputFields['inputMaxVolume'].value,
+                    'inputH2O': self.inputFields['inputH2O'].value,
+                    'inputBuffer': self.inputFields['inputBuffer'].value,
+                    'inputEnhancer': self.inputFields['inputEnhancer'].value,
+                    'inputMgCl2': self.inputFields['inputMgCl2'].value,
+                    'inputPrimer1': self.inputFields['inputPrimer1'].value,
+                    'inputPrimer2': self.inputFields['inputPrimer2'].value,
+                    'inputPolymerase': self.inputFields['inputPolymerase'].value,
+                    'inputDNTPs': self.inputFields['inputDNTPs'].value,
+                    'inputDNA': self.inputFields['inputDNA'].value,
+                    'outputMaxVolume': self.inputFields['outputMaxVolume'].value,
+                    'numberOfProbes': self.inputFields['numberOfProbes'].value,
+                    'inputDifferenceValue': self.resultFields['inputDifferenceValue'].innerHTML,
+                    'outputH2O': self.resultFields['outputH2O'].innerHTML,
+                    'outputH2OMax': self.resultFields['outputH2OMax'].innerHTML,
+                    'outputBuffer': self.resultFields['outputBuffer'].innerHTML,
+                    'outputBufferMax': self.resultFields['outputBufferMax'].innerHTML,
+                    'outputEnhancer': self.resultFields['outputEnhancer'].innerHTML,
+                    'outputEnhancerMax': self.resultFields['outputEnhancerMax'].innerHTML,
+                    'outputMgCl2': self.resultFields['outputMgCl2'].innerHTML,
+                    'outputMgCl2Max': self.resultFields['outputMgCl2Max'].innerHTML,
+                    'outputPrimer1': self.resultFields['outputPrimer1'].innerHTML,
+                    'outputPrimer1Max': self.resultFields['outputPrimer1Max'].innerHTML,
+                    'outputPrimer2': self.resultFields['outputPrimer2'].innerHTML,
+                    'outputPrimer2Max': self.resultFields['outputPrimer2Max'].innerHTML,
+                    'outputDNTPs': self.resultFields['outputDNTPs'].innerHTML,
+                    'outputDNTPsMax': self.resultFields['outputDNTPsMax'].innerHTML,
+                    'outputPolymerase': self.resultFields['outputPolymerase'].innerHTML,
+                    'outputPolymeraseMax': self.resultFields['outputPolymeraseMax'].innerHTML,
+                    'outputDNA': self.resultFields['outputDNA'].innerHTML
+                };
+                let history = localStorage.getItem("pcrCalcHistory");
+                if (history === null) {
+                    localStorage.setItem("pcrCalcHistory", "["+JSON.stringify(self.dataForStorage['fields'])+"] ");
+                } else {
+                    let h = JSON.parse(history);
+                    h.push(self.dataForStorage['fields']);
+                    localStorage.setItem("pcrCalcHistory", JSON.stringify(h));
+                } 
+            } else {
+                console.error("Ta przeglądarka nie obsługuje localStorage!");
+            }            
+        });
     }
 
     public init() {
         this.recalc();
+        this.addListener();
         var self = this;
         for (var property in this.inputFields) {
             if (this.inputFields.hasOwnProperty(property)) {
