67be511635b510ce2bf7373b97c05dcdfd7fb5ae
szczypiorofix <szczypiorofix@o2.pl>
2018-05-30 17:13:55 +0200

    Poprawki które niby mają rozwiązać problem.

--- js/pwa.js
+++ js/pwa.js
@@ -2,16 +2,15 @@ if ('serviceWorker' in navigator && 'PushManager' in window) {
     navigator.serviceWorker.register('./service-worker.js').then(function (reg) {
         console.log('Service Worker Registered.');
         //console.log(reg.pushManager);
-        reg.pushManager.getSubscription().then(function (sub) {
-            if (sub === null) {
-                // Update UI to ask user to register for Push
-                console.log('Not subscribed to push service!');
-            }
-            else {
-                // We have a subscription, update the database
-                console.log('Subscription object: ', sub);
-            }
-        });
+        // reg.pushManager.getSubscription().then(function(sub) {
+        //     if (sub === null) {
+        //         // Update UI to ask user to register for Push
+        //         console.log('Not subscribed to push service!');
+        //     } else {
+        //         // We have a subscription, update the database
+        //         console.log('Subscription object: ', sub);
+        //     }
+        // });
     }).catch(function (err) {
         console.error("Service Worker registration failed: " + err);
     });
@@ -19,35 +18,32 @@ if ('serviceWorker' in navigator && 'PushManager' in window) {
 else {
     console.warn('Push messaging and Service Worker is not supported');
 }
-let deferredPrompt;
-let btnAdd = document.getElementById("offline-button-yes");
-window.addEventListener('beforeinstallprompt', (e) => {
-    //e.preventDefault();
-    console.log('Teraz powinno się pojawić zastępcze powiadomienie o możliwości dodania do homescreen');
-    deferredPrompt = e;
-    console.log(deferredPrompt);
-    document.getElementById('offline-div').style.display = 'block';
-});
-btnAdd.addEventListener('click', (e) => {
-    // hide our user interface that shows our A2HS button
-    // btnAdd.style.display = 'none';
-    // Show the prompt
-    console.log(deferredPrompt);
-    if (deferredPrompt !== undefined || deferredPrompt !== null) {
-        deferredPrompt.prompt();
-        // Wait for the user to respond to the prompt
-        deferredPrompt.userChoice
-            .then((choiceResult) => {
-            if (choiceResult.outcome === 'accepted') {
-                console.log('User accepted the A2HS prompt');
-            }
-            else {
-                console.log('User dismissed the A2HS prompt');
-            }
-            deferredPrompt = null;
-        });
-    }
-});
+// let deferredPrompt;
+// var btnAdd = document.getElementById("offline-button-yes");
+// var offlineDiv = document.getElementById("offline-div");
+// window.addEventListener('beforeinstallprompt', (e) => {
+//     console.log('Odpalamy!');
+//     //e.preventDefault();
+//     deferredPrompt = e;
+//     // Update UI notify the user they can add to home screen
+//     //offlineDiv.style.display = 'flex';
+// });
+// btnAdd.addEventListener('click', (e) => {
+//     // hide our user interface that shows our A2HS button
+//     offlineDiv.style.display = 'none';
+//     // Show the prompt
+//     deferredPrompt.prompt();
+//     // Wait for the user to respond to the prompt
+//     deferredPrompt.userChoice
+//       .then((choiceResult) => {
+//         if (choiceResult.outcome === 'accepted') {
+//           console.log('User accepted the A2HS prompt');
+//         } else {
+//           console.log('User dismissed the A2HS prompt');
+//         }
+//         deferredPrompt = null;
+//       });
+//   });
 // document.getElementById("saveoffline").addEventListener('click', function(event) {
 //     event.preventDefault();
 //     caches.open('PCR-calc.v-1.00.000').then(function(cache) {
