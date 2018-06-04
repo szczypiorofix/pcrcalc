8b8d12409b3fc6e1cbe8f76ebe396bcbdc7fcc55
szczypiorofix <szczypiorofix@o2.pl>
2018-06-04 15:17:18 +0200

    Drobne poprawki i usprawnienia.

--- js/sw.js
+++ js/sw.js
@@ -12,15 +12,32 @@ else {
 }
 let deferredPrompt;
 let offlineDiv = document.getElementById('offline-div');
+// chrome://apps
+document.getElementById('offline-button-no').addEventListener('click', function (e) {
+    offlineDiv.style.display = 'none';
+});
+// TO CO PONIŻEJ ZROBIĆ NA PROMISACH
 window.addEventListener('beforeinstallprompt', (e) => {
-    // Prevent Chrome 67 and earlier from automatically showing the prompt
     e.preventDefault();
-    // Stash the event so it can be triggered later.
     deferredPrompt = e;
     console.log('Tutaj powinna pojawiać się informacja o dodaniu do offline.');
     offlineDiv.style.display = 'flex';
 });
-document.getElementById('offline-button-no').addEventListener('click', function (e) {
+document.getElementById('offline-button-yes').addEventListener('click', (e) => {
+    // hide our user interface that shows our A2HS button
     offlineDiv.style.display = 'none';
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice
+        .then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the A2HS prompt');
+        }
+        else {
+            console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+    });
 });
 //# sourceMappingURL=sw.js.map
